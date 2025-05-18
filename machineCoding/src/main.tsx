3daa804d10b3c9ac366c746ba561113174424140
Rishabh Shukla <rsh.7964@gmail.com>
2025-05-18 19:51:39 +0530

    ga4

--- machineCoding/src/main.tsx
+++ machineCoding/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
 import "./index.css";
 import App from "./App.tsx";
 import "./utils/i18n.ts";
 
+ReactGA.initialize("G-LSVFYLD40C");
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
